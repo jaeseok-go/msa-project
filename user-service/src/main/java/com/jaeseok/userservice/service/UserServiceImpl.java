07850f0b9829945d64f95ddab2111860bb128a19
jaeseok-go <gojeasuk2@naver.com>
2021-07-26 16:48:19 +0900

    feat: spring security 적용 및 비밀번호 암호화
    Spring Security 적용하여 특정 url만 permit
    BCryptPasswordEncoder로 비밀번호 암호화하여 저장

--- user-service/src/main/java/com/jaeseok/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/jaeseok/userservice/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.jaeseok.userservice.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService{
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public UserDto createdUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
@@ -24,7 +28,8 @@ public class UserServiceImpl implements UserService{
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPassword("encrypted_password");
+        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+        userEntity.setEncryptedPassword(encodedPassword);
 
         userRepository.save(userEntity);
 
