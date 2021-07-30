3ab20db9a239f9a83fd315b4b58ab60b8492c836
jaeseok-go <gojeasuk2@naver.com>
2021-07-30 11:57:23 +0900

    feat: jwt 생성
    application.yml 파일에 token 정보 명시
    jwt 디팬던시 추가 및 생성 코드 작성

--- user-service/src/main/java/com/jaeseok/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/jaeseok/userservice/security/AuthenticationFilter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jaeseok.userservice.dto.UserDto;
 import com.jaeseok.userservice.service.UserService;
 import com.jaeseok.userservice.vo.RequestLogin;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -19,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -53,5 +57,17 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         User user = ((User)authResult.getPrincipal());
         String userName = user.getUsername();
         UserDto userDetails = userService.getUserDetailsByEmail(userName);
+
+        long expirationPeriod = Long.parseLong(environment.getProperty("token.expiration_time"));
+        String secret = environment.getProperty("token.secret");
+
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationPeriod))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
     }
 }
